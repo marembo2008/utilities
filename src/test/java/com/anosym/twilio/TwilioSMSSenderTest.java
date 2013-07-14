153ada1f7b5090cde65513b9305b60455068b706
kenn <kennmunene@gmail.com>
2013-07-14 20:29:48 +0300

    removing munene duplication

--- src/test/java/com/anosym/twilio/TwilioSMSSenderTest.java
+++ src/test/java/com/anosym/twilio/TwilioSMSSenderTest.java
@@ -29,10 +29,10 @@ public class TwilioSMSSenderTest {
   @Test
   public void testSendSMS() {
 
-//    String twilioConfig = "/home/marembo";
-//    System.setProperty("twilio.config", twilioConfig);
-//    TwilioSMSSender sender = TwilioSMSSender.getInstance();
-//boolean result = sender.sendSMS("+254726153577", "Hello this is a twilio sms test");
+    String twilioConfig = "/home/kenn";
+    System.setProperty("twilio.config", twilioConfig);
+    TwilioSMSSender sender = TwilioSMSSender.getInstance();
+//boolean result = sender.sendSMS("+254725214698", "Hello this is a twilio sms test");
     boolean result = true;
       assertTrue(result);
 
