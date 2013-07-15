e35b7447c5d2b2f80033e5ef88ad3e25e06830da
kenn <kennmunene@gmail.com>
2013-07-15 12:51:29 +0300

    adjusting for munene

--- src/test/java/com/anosym/twilio/TwilioSMSSenderTest.java
+++ src/test/java/com/anosym/twilio/TwilioSMSSenderTest.java
@@ -28,14 +28,10 @@ public class TwilioSMSSenderTest {
 
   @Test
   public void testSendSMS() {
-<<<<<<< HEAD
 
     String twilioConfig = "/home/kenn";
     System.setProperty("twilio.config", twilioConfig);
-=======
-    String twilioConfig = "/home/marembo";
     System.setProperty(TwilioSMSSender.TWILIO_CONFIG_PROPERTY, twilioConfig);
->>>>>>> a08cc849dca04fd2b2989f040c708a00aa879e47
     TwilioSMSSender sender = TwilioSMSSender.getInstance();
 //boolean result = sender.sendSMS("+254725214698", "Hello this is a twilio sms test");
     boolean result = true;
