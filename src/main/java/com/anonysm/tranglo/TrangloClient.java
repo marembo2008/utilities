426749e08f701b9b224856fdcdf93b0a211c9b86
kenn <kennmunene@gmail.com>
2013-08-03 23:10:22 +0300

    airtime send

--- src/main/java/com/anonysm/tranglo/TrangloClient.java
+++ src/main/java/com/anonysm/tranglo/TrangloClient.java
@@ -12,25 +12,89 @@ import com.anonysm.tranglo.service.EPinReloadSoap;
  * @author kenn
  */
 public class TrangloClient {
-     static final String username = "afrocoin_test";
-    static final String password = "282784";
-    static final String transID = "0349848";
-    
-    public static void main(String[] args) {
-        EPinReload ep = new EPinReload();
-        EPinReloadSoap soap = ep.getEPinReloadSoap();
-//        int res = soap.ping();
-//        System.out.println("ping = "+ res);        
+
+    public static final String TRANGLO_CONFIG_PROPERTY = TrangloConfigUtil.TRANGLO_CONFIG_PROPERTY;
+    TrangloConfiguration trangloConfig;
+    private static TrangloClient sender;
+
+    EPinReload serviceDescriptor = null;
+    EPinReloadSoap service = null;
+    String username = null;
+    String password = null;
+    String transID = null;
+    String prodCode = "";
+    public boolean sendAirtime(String from, String to, int amount){
         
-        String souceNo = "255710214698";
-        String prodCode = "";
-        String destNo = souceNo;
-        int deno = 1500;
-        String res =soap.requestReload(souceNo, destNo, prodCode, deno, username, password, transID);
-       // soap.requestReload(souceNo, destNo, prodCode, deno, username, password, transID);
-         System.out.println("res "+res);
+        String responseCode = service.requestReload(from, to, prodCode, amount, username, password, transID);
+        System.out.println("response code "+responseCode);
+        try {
+           int response = Integer.parseInt(responseCode);
+            if(response == 0 || response == 1){
+                return true;
+            }
+        } catch (Exception e) {
+            
+        }
+        return false;
+    }
+    
+    private void init(){
+        serviceDescriptor = new EPinReload();
+        service = serviceDescriptor.getEPinReloadSoap();
+        service.ping();
+        username = trangloConfig.getUsername();
+        password = trangloConfig.getPassword();
+        //TODO confirm with Andre how this should work
+        transID = "12345";
     }
     
     
+    public TrangloClient(TrangloConfiguration trangloConfig) {
+        this.trangloConfig = trangloConfig;
+        init();
+    }
+
+    private TrangloClient() {
+        this.trangloConfig = TrangloConfigUtil.getTwilioConfig();
+        if (trangloConfig == null) {
+            String err = "tranglo config has not been specified: "
+                    + "Tranglo config path=" + System.getProperty(TRANGLO_CONFIG_PROPERTY, System.getProperty("user.home"));
+            throw new RuntimeException(err);
+        }
+        init();
+    }
+
+    public static TrangloClient getInstance() throws IllegalStateException {
+        if (sender == null) {
+            sender = new TrangloClient();
+        }
+        return sender;
+    }
+
+    public static TrangloClient getInstance(TrangloConfiguration config) throws IllegalStateException {
+        if (sender == null) {
+            sender = new TrangloClient(config);
+        }
+        return sender;
+    }
+
+
     
+    public static void main(String[] args) {
+        TrangloClient tc = TrangloClient.getInstance();
+        String tzNumber = "255710437287";
+        String saNumber = "27763574581";
+        //TODO marembo test these numbers, sa does not work, tz works
+        String from = saNumber;
+        String to = from;
+        int amount = 1500;
+       boolean res =  tc.sendAirtime(from, to, amount);
+       if(res){
+           System.out.println("SENT AIRTIME TO "+to);
+       }else{
+           System.out.println("FAILED to send airtime to "+ to);
+       }
+           
+        
+    }
 }
