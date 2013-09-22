db92bd00f4c5fd035a3e71b1df4ae8996dd1824d
Marembo Ochieng <marembo2008@gmail.com>
2013-09-22 22:04:42 +0200

    s

--- src/main/java/com/anosym/twilio/TwilioSMSSender.java
+++ src/main/java/com/anosym/twilio/TwilioSMSSender.java
@@ -8,6 +8,7 @@ import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.resource.factory.SmsFactory;
 import com.twilio.sdk.resource.instance.Sms;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -19,6 +20,35 @@ import java.util.logging.Logger;
  */
 public class TwilioSMSSender {
 
+  public static class SmsResult implements Serializable {
+
+    private boolean success;
+    private String message;
+
+    public SmsResult() {
+    }
+
+    public SmsResult(boolean success, String message) {
+      this.success = success;
+      this.message = message;
+    }
+
+    public boolean isSuccess() {
+      return success;
+    }
+
+    public void setSuccess(boolean success) {
+      this.success = success;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+  }
   /**
    * This is just to provide an easire access to the loading of the configs.
    */
@@ -52,7 +82,27 @@ public class TwilioSMSSender {
     return sender;
   }
 
-  public boolean sendSMS(String number, String msg) {
+  public boolean sendSMS(String number, String msg) throws TwilioRestException {
+    /**
+     * The number must start with +
+     */
+    if (!number.startsWith("+")) {
+      number += "+";
+    }
+    //if the message is more than 160 character, truncate it!!
+    msg = msg.length() > 160 ? msg.substring(0, 160) : msg;
+    TwilioRestClient client = new TwilioRestClient(config.getAccountSid(), config.getAuthToken());
+    Map<String, String> params = new HashMap<String, String>();
+    params.put("Body", msg);
+    params.put("To", number);
+    params.put("From", config.getFromPhoneNumber());
+    SmsFactory messageFactory = client.getAccount().getSmsFactory();
+    Sms message = messageFactory.create(params);
+    Logger.getLogger(TwilioSMSSender.class.getName()).log(Level.INFO, "Sent SMS: {0}", number + ", " + msg + ", " + message);
+    return true;
+  }
+
+  public SmsResult sendSms(String number, String msg) {
     /**
      * The number must start with +
      */
@@ -71,10 +121,10 @@ public class TwilioSMSSender {
     try {
       message = messageFactory.create(params);
       Logger.getLogger(TwilioSMSSender.class.getName()).log(Level.INFO, "Sent SMS: {0}", number + ", " + msg + ", " + message);
+      return new SmsResult(true, message.getStatus());
     } catch (TwilioRestException e) {
       Logger.getLogger(TwilioSMSSender.class.getName()).log(Level.SEVERE, "Send SMS FAIL: " + number + ", " + msg + ", " + message, e);
-      return false;
+      return new SmsResult(false, e.getErrorMessage() + ": " + e.getMoreInfo());
     }
-    return true;
   }
 }
