bcf4468b9e3948e4fa5a2d5da2e207fb07b9492e
Marembo Ochieng <marembo2008@gmail.com>
2014-11-14 22:47:07 +0100

    no printerr

--- src/main/java/com/anosym/utilities/geocode/google/GoogleLocation.java
+++ src/main/java/com/anosym/utilities/geocode/google/GoogleLocation.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.anosym.utilities.geocode.google;
 
 import java.io.Serializable;
@@ -12,36 +8,36 @@ import java.io.Serializable;
  */
 public class GoogleLocation implements Serializable {
 
-  private static final long serialVersionUID = 4824482249201L;
-  private double lat;
-  private double lng;
+    private static final long serialVersionUID = 4824482249201L;
+    private double lat;
+    private double lng;
 
-  public GoogleLocation(double lat, double lng) {
-    this.lat = lat;
-    this.lng = lng;
-  }
+    public GoogleLocation(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
 
-  public GoogleLocation() {
-  }
+    public GoogleLocation() {
+    }
 
-  public double getLat() {
-    return lat;
-  }
+    public double getLat() {
+        return lat;
+    }
 
-  public void setLat(double lat) {
-    this.lat = lat;
-  }
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
 
-  public double getLng() {
-    return lng;
-  }
+    public double getLng() {
+        return lng;
+    }
 
-  public void setLng(double lng) {
-    this.lng = lng;
-  }
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
 
-  @Override
-  public String toString() {
-    return lat + "," + lng;
-  }
+    @Override
+    public String toString() {
+        return lat + "," + lng;
+    }
 }
