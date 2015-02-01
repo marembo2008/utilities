b50faa421c551572ecb34927aba4aa8ec5ffdcc7
Marembo Ochieng <marembo2008@gmail.com>
2015-02-01 18:15:42 +0100

    Amount equals logic

--- src/main/java/com/anosym/utilities/currency/Amount.java
+++ src/main/java/com/anosym/utilities/currency/Amount.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.anosym.utilities.currency;
 
 import com.anosym.utilities.formatter.CurrencyFormatter;
 import com.anosym.utilities.IdGenerator;
 import com.anosym.utilities.jaxb.CurrencyJAXBAdapter;
+import com.google.common.base.Objects;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -19,6 +16,8 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
 /**
  *
  * @author marembo
@@ -27,180 +26,180 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @XmlRootElement(name = "Amount")
 public class Amount implements Serializable, Comparable<Amount> {
 
-  public static final String DEFAULT_ENVIRONMENT_COUNTRY = "default.environment.country";
-  public static final String DEFAULT_ENVIRONMENT_LANGUAGE = "default.environment.language";
-  private static final long serialVersionUID = IdGenerator.serialVersionUID(Amount.class);
-  /**
-   * Normal use cases
-   */
-  public static final Amount ZERO = new Amount();
-  /**
-   * member instances
-   */
-  private BigDecimal value;
-  @XmlJavaTypeAdapter(CurrencyJAXBAdapter.class)
-  private Currency currency;
+    public static final String DEFAULT_ENVIRONMENT_COUNTRY = "default.environment.country";
+    public static final String DEFAULT_ENVIRONMENT_LANGUAGE = "default.environment.language";
+    private static final long serialVersionUID = IdGenerator.serialVersionUID(Amount.class);
+    /**
+     * Normal use cases
+     */
+    public static final Amount ZERO = new Amount();
+    /**
+     * member instances
+     */
+    private BigDecimal value;
+    @XmlJavaTypeAdapter(CurrencyJAXBAdapter.class)
+    private Currency currency;
 
-  public Amount() {
-    this(BigDecimal.ZERO);
-  }
-
-  public Amount(Locale locale) {
-    this(BigDecimal.ZERO, Currency.getInstance(locale));
-  }
-
-  public Amount(BigDecimal value) {
-    this(value, getDefault());
-  }
-
-  public Amount(BigDecimal value, Currency currency) {
-    this.value = value;
-    this.currency = currency;
-  }
-
-  public Amount(BigDecimal value, Locale locale) {
-    this(value, Currency.getInstance(locale));
-  }
-
-  private static Currency getDefault() {
-    String language = System.getProperty(DEFAULT_ENVIRONMENT_LANGUAGE, "en_US");
-    String country = System.getProperty(DEFAULT_ENVIRONMENT_COUNTRY, "US");
-    try {
-      return Currency.getInstance(new Locale(language, country));
-    } catch (Exception e) {
-      Logger.getLogger(Amount.class.getName()).log(Level.SEVERE, language, e);
-      return Currency.getInstance(new Locale("en_US", "US"));
+    public Amount() {
+        this(BigDecimal.ZERO);
     }
-  }
 
-  public Amount getPercentage(float percentage) {
-    BigDecimal percentageValue = this.value.multiply(BigDecimal.valueOf(percentage));
-    return new Amount(percentageValue.divide(BigDecimal.valueOf(100), RoundingMode.UP), currency);
-  }
-
-  public Amount getPercentage(BigDecimal percentage) {
-    BigDecimal percentageValue = this.value.multiply(percentage);
-    return new Amount(percentageValue, currency);
-  }
-
-  public BigDecimal getValue() {
-    if (value != null) {
-      value = value.setScale(2, RoundingMode.UP);
+    public Amount(Locale locale) {
+        this(BigDecimal.ZERO, Currency.getInstance(locale));
     }
-    return value;
-  }
 
-  public void setValue(BigDecimal value) {
-    this.value = value;
-  }
-
-  public Currency getCurrency() {
-    return currency;
-  }
-
-  public void setCurrency(Currency currency) {
-    this.currency = currency;
-  }
-
-  public Amount add(Amount amount) {
-    if (this.value == null || amount.value == null) {
-      throw new ArithmeticException("Amount value undefined");
+    public Amount(BigDecimal value) {
+        this(value, getDefault());
     }
-    BigDecimal amountValue = amount.value;
-    if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
-      // we use the currency code, since they may be referring to different countries
-      CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
-      if (ccer != null) {
-        amountValue = amountValue.multiply(ccer.getFxRate());
-      }
-    }
-    return new Amount(this.value.add(amountValue), currency);
-  }
 
-  public Amount subtract(Amount amount) {
-    if (this.value == null || amount.value == null) {
-      throw new ArithmeticException("Amount value undefined");
+    public Amount(BigDecimal value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
     }
-    BigDecimal amountValue = amount.value;
-    if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
-      // we use the currency code, since they may be referring to different countries
-      CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
-      if (ccer != null) {
-        amountValue = amountValue.multiply(ccer.getFxRate());
-      }
-    }
-    return new Amount(this.value.subtract(amountValue), currency);
-  }
 
-  public Amount negate() {
-    if (this.value == null) {
-      throw new ArithmeticException("Amount value undefined");
+    public Amount(BigDecimal value, Locale locale) {
+        this(value, Currency.getInstance(locale));
     }
-    return new Amount(this.value.negate(), currency);
-  }
 
-  public Amount divide(Amount amount, RoundingMode roundingMode) {
-    if (this.value == null || amount.value == null) {
-      throw new ArithmeticException("Amount value undefined");
+    private static Currency getDefault() {
+        String language = System.getProperty(DEFAULT_ENVIRONMENT_LANGUAGE, "en_US");
+        String country = System.getProperty(DEFAULT_ENVIRONMENT_COUNTRY, "US");
+        try {
+            return Currency.getInstance(new Locale(language, country));
+        } catch (Exception e) {
+            Logger.getLogger(Amount.class.getName()).log(Level.SEVERE, language, e);
+            return Currency.getInstance(new Locale("en_US", "US"));
+        }
     }
-    BigDecimal amountValue = amount.value;
-    if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
-      // we use the currency code, since they may be referring to different countries
-      CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
-      if (ccer != null) {
-        amountValue = amountValue.multiply(ccer.getFxRate());
-      }
+
+    public Amount getPercentage(float percentage) {
+        BigDecimal percentageValue = this.value.multiply(BigDecimal.valueOf(percentage));
+        return new Amount(percentageValue.divide(BigDecimal.valueOf(100), RoundingMode.UP), currency);
     }
-    return new Amount(value.divide(amountValue, roundingMode), currency);
-  }
 
-  public static Amount valueOf(Number value) {
-    return new Amount(BigDecimal.valueOf(value.doubleValue()));
-  }
-
-  public static Amount valueOf(Number value, Currency currency) {
-    return new Amount(BigDecimal.valueOf(value.doubleValue()), currency);
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 7;
-    hash = 89 * hash + (this.value != null ? this.value.hashCode() : 0);
-    hash = 89 * hash + (this.currency != null ? this.currency.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
+    public Amount getPercentage(BigDecimal percentage) {
+        BigDecimal percentageValue = this.value.multiply(percentage);
+        return new Amount(percentageValue, currency);
     }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final Amount other = (Amount) obj;
-    if (this.value != other.value && (this.value == null || !this.value.equals(other.value))) {
-      return false;
-    }
-    return this.currency == other.currency || (this.currency != null && this.currency.equals(other.currency));
-  }
 
-  @Override
-  public String toString() {
-    return this.currency + " " + new CurrencyFormatter().format(getValue());
-  }
-
-  @Override
-  public int compareTo(Amount amount) {
-    BigDecimal amountValue = amount.value;
-    if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode()) && amountValue.compareTo(BigDecimal.ZERO) != 0) {
-      // we use the currency code, since they may be referring to different countries
-      //of course any zero value in any currency is equivalent,
-      CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
-      if (ccer != null) {
-        amountValue = amountValue.multiply(ccer.getFxRate());
-      }
+    public BigDecimal getValue() {
+        if (value != null) {
+            value = value.setScale(2, RoundingMode.UP);
+        }
+        return value;
     }
-    return this.value.compareTo(amountValue);
-  }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Amount add(Amount amount) {
+        if (this.value == null || amount.value == null) {
+            throw new ArithmeticException("Amount value undefined");
+        }
+        BigDecimal amountValue = amount.value;
+        if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
+            // we use the currency code, since they may be referring to different countries
+            CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
+            if (ccer != null) {
+                amountValue = amountValue.multiply(ccer.getFxRate());
+            }
+        }
+        return new Amount(this.value.add(amountValue), currency);
+    }
+
+    public Amount subtract(Amount amount) {
+        if (this.value == null || amount.value == null) {
+            throw new ArithmeticException("Amount value undefined");
+        }
+        BigDecimal amountValue = amount.value;
+        if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
+            // we use the currency code, since they may be referring to different countries
+            CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
+            if (ccer != null) {
+                amountValue = amountValue.multiply(ccer.getFxRate());
+            }
+        }
+        return new Amount(this.value.subtract(amountValue), currency);
+    }
+
+    public Amount negate() {
+        if (this.value == null) {
+            throw new ArithmeticException("Amount value undefined");
+        }
+        return new Amount(this.value.negate(), currency);
+    }
+
+    public Amount divide(Amount amount, RoundingMode roundingMode) {
+        if (this.value == null || amount.value == null) {
+            throw new ArithmeticException("Amount value undefined");
+        }
+        BigDecimal amountValue = amount.value;
+        if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode())) {
+            // we use the currency code, since they may be referring to different countries
+            CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
+            if (ccer != null) {
+                amountValue = amountValue.multiply(ccer.getFxRate());
+            }
+        }
+        return new Amount(value.divide(amountValue, roundingMode), currency);
+    }
+
+    public static Amount valueOf(Number value) {
+        return new Amount(BigDecimal.valueOf(value.doubleValue()));
+    }
+
+    public static Amount valueOf(Number value, Currency currency) {
+        return new Amount(BigDecimal.valueOf(value.doubleValue()), currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.value, currency);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Amount other = (Amount) obj;
+        /*
+         * This is a hack, based on how Equality for BigDecimal occurs which may fail for BigDecimal.valueOf(100.00) and BigDecimal.valueOf(100)
+         */
+        final BigDecimal thisValue = firstNonNull(this.value, BigDecimal.ZERO);
+        final BigDecimal otherValue = firstNonNull(other.value, BigDecimal.ZERO);
+        return thisValue.compareTo(otherValue) == 0
+                && Objects.equal(this.currency, other.currency);
+    }
+
+    @Override
+    public String toString() {
+        return this.currency + " " + new CurrencyFormatter().format(getValue());
+    }
+
+    @Override
+    public int compareTo(Amount amount) {
+        BigDecimal amountValue = amount.value;
+        if (!this.currency.getCurrencyCode().equals(amount.currency.getCurrencyCode()) && amountValue.compareTo(BigDecimal.ZERO) != 0) {
+            // we use the currency code, since they may be referring to different countries
+            //of course any zero value in any currency is equivalent,
+            CurrentCurrencyExchangeRate ccer = CurrentCurrencyExchangeRateManager.getCurrentCurrencyExchangeRate(amount.currency, this.currency);
+            if (ccer != null) {
+                amountValue = amountValue.multiply(ccer.getFxRate());
+            }
+        }
+        return this.value.compareTo(amountValue);
+    }
 }
